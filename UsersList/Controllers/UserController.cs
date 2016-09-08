2fc3c3e446867771f9cfbb3a6cc19a8000e27abd
vega7_000 <vega7_000@VOYAGER>
2016-09-08 22:59:47 +0300

    08.09.2016

--- UsersList/Controllers/UserController.cs
+++ UsersList/Controllers/UserController.cs
@@ -20,40 +20,56 @@ namespace UsersList.Controllers
         }
 
         [HttpPost]
-        public ActionResult Add(User user, HttpPostedFileBase image)
+        //[ValidateAntiForgeryToken]
+        public ActionResult Add()
         {
-            if (ModelState.IsValid)
+            User userFromDb;
+
+            if (!ModelState.IsValid) return Json(new {res = false});
+
+            HttpPostedFileBase image = Request.Files["userAvatar"];
+                
+            var userIdWeb = Request.Form[0];
+
+            if (userIdWeb == "")
             {
+                userFromDb = new User
+                {
+                    UserName = Request.Form["userName"],
+                    UserEmail = Request.Form["userEmail"],
+                    SkypeLogin = Request.Form["skypeLogin"],
+                    Signature = Request.Form["signature"]
+                };
+
                 if (image != null)
                 {
-                    user.ImageMimeType = image.ContentType;
-                    user.UserAvatar = new byte[image.ContentLength];
-                    image.InputStream.Read(user.UserAvatar, 0, image.ContentLength);
+                    userFromDb.ImageMimeType = image.ContentType;
+                    userFromDb.UserAvatar = new byte[image.ContentLength];
+                    image.InputStream.Read(userFromDb.UserAvatar, 0, image.ContentLength);
                 }
-                Utils.UtilsUser.UpdateUserByModelFronWeb(_db, ref user);
             }
-            return Json(new { res = true});
-        }
-
-        [HttpPost]
-        public ActionResult AddImage(HttpPostedFileBase image)
-        {
-            //var user = _db.Users.Find(userId);
-
-            if (ModelState.IsValid)
+            else
             {
-            //    if (image != null)
-            //    {
-            //        user.ImageMimeType = image.ContentType;
-            //        user.UserAvatar = new byte[image.ContentLength];
-            //        image.InputStream.Read(user.UserAvatar, 0, image.ContentLength);
-            //    }
-            //    Utils.UtilsUser.UpdateUserByModelFronWeb(_db, ref user);
+                userFromDb = _db.Users.Find(int.Parse(userIdWeb));
+
+                userFromDb.UserName = Request.Form["userName"];
+                userFromDb.UserEmail = Request.Form["userEmail"];
+                userFromDb.SkypeLogin = Request.Form["skypeLogin"];
+                userFromDb.Signature = Request.Form["signature"];
+
+                if (image != null)
+                {
+                    userFromDb.ImageMimeType = image.ContentType;
+                    userFromDb.UserAvatar = new byte[image.ContentLength];
+                    image.InputStream.Read(userFromDb.UserAvatar, 0, image.ContentLength);
+                }
             }
-            return Json(new { res = true });
+
+            Utils.UtilsUser.UpdateUserByModelFronWeb(_db, ref userFromDb);
+
+            return Json(new {res = true});
         }
-
-
+        
         public FileContentResult GetImage(int userId)
         {
             User userImage = _db.Users.FirstOrDefault(p => p.UserId == userId);
